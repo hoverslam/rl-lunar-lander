2ceae7224bebe0211f80d982f2252213f7ef87b4
Christopher Hofmann <hofmann.christopher@outlook.com>
2022-08-25 14:36:57 +0200

    Make agent an argument

--- main.py
+++ main.py
@@ -3,32 +3,34 @@ import sys
 
 sys.path.append("modules")
 from nfq import NFQAgent
+from dqn import DQNAgent
 
 
 agents = {"NFQ": NFQAgent(gamma=0.99, epsilon_init=1.0, epsilon_min=0.1, epsilon_decay=0.9, 
-                          alpha=0.0001, input_dim=8, output_dim=4, hidden_dims=[1028, 512])}
+                          alpha=0.0001, input_dim=8, output_dim=4, hidden_dims=[1028, 512], 
+                          epochs=50)}
 
 
-def train(episodes: int) -> None:
+def train(episodes: int, algo: str) -> None:
     env = gym.make("LunarLander-v2", new_step_api=True)
 
-    a = agents["NFQ"]
-    results = a.train(env, episodes, 50)
+    a = agents[algo]
+    results = a.train(env, episodes)
     a.plot_results(results, a.name)
-    a.save_model("nfq.pt")
+    a.save_model("{}.pt".format(algo))
     
     env.close()
     
-def show(episodes: int) -> None:
+def show(episodes: int, algo: str) -> None:
     env = gym.make("LunarLander-v2", render_mode="human", new_step_api=True)
         
-    a = agents["NFQ"]
-    a.load_model("nfq.pt")
+    a = agents[algo]
+    a.load_model("{}.pt".format(algo))
     a.play(env, episodes)
     
     env.close()
     
 
 if __name__ == "__main__":
-    train(1000)
-    show(10)
+    train(1000, "NFQ")
+    show(10, "NFQ")
