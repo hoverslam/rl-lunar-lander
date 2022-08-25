932897d42cfc57857e4e1f78a0bbe0c055fba905
hoverslam <hofmann.christopher@outlook.com>
2022-08-25 19:56:22 +0200

    Add docstrings

--- modules/dqn.py
+++ modules/dqn.py
@@ -41,8 +41,16 @@ class DQNAgent(Agent):
         self.memory = ReplayMemory(max_size=memory_size)
         self.batch_size = batch_size
         
-    def train(self, env, episodes):
-        # TODO: Test
+    def train(self, env, episodes) -> dict:
+        """Train the agent on a given number of episodes.
+
+        Args:
+            env(_type_): An OpenAI gym environment.
+            episodes (int): Number of episodes.
+
+        Returns:
+            dict: A dictionary containing the score of each episode.
+        """
         self.model.train()
         epsilons = self.decay_schedule(self.epsilon_init, self.epsilon_min, self.epsilon_decay, episodes)
         results = {"episode": [], "score": []}
@@ -69,8 +77,10 @@ class DQNAgent(Agent):
             
         return results
     
-    def optimize(self):
-        # TODO: Test
+    def optimize(self) -> None:
+        """Fit agent model.
+        """
+        # TODO: Implement target network to make training (much) more stable.
         states, actions, rewards, next_states, terminated = self.memory.sample(self.batch_size)
         states = torch.tensor(states, dtype=torch.float32, device=self.model.device)
         actions = torch.tensor(actions, dtype=torch.int64, device=self.model.device).unsqueeze(1)
@@ -90,9 +100,10 @@ class DQNAgent(Agent):
    
 class DQN(nn.Module):
     
-    def __init__(self, input_dim: int, output_dim: int, hidden_dims: list[int]):
+    def __init__(self, input_dim: int, output_dim: int, hidden_dims: list[int]) -> None:
+        """Initialize model.
+        """  
         super(DQN, self).__init__()        
-        # TODO: Test
         self.input_layer = nn.Linear(input_dim, hidden_dims[0])
         self.hidden_layers = nn.ModuleList()
         for i in range(len(hidden_dims)-1):
@@ -103,8 +114,15 @@ class DQN(nn.Module):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.to(self.device)
     
-    def forward(self, state):
-        # TODO: Test
+    def forward(self, state) -> torch.Tensor:
+        """Make a prediction given an observation.
+
+        Args:
+            state (np.ndarray): Observation of the environment.
+
+        Returns:
+            torch.Tensor: Q-values for a given observation.
+        """
         if not isinstance(state, torch.Tensor):
             x = torch.tensor(state, dtype=torch.float32, device=self.device)
         else:
@@ -119,8 +137,13 @@ class DQN(nn.Module):
     
     
 class ReplayMemory():
-    # TODO: Write documentation
-    def __init__(self, max_size):
+    
+    def __init__(self, max_size: int) -> None:
+        """Initialize memory.
+
+        Args:
+            max_size (int): Maximum size of memory.
+        """
         self.max_size = max_size
         self.size = 0
         self.index = 0
@@ -131,21 +154,40 @@ class ReplayMemory():
         self.rewards = [None] * max_size
         self.next_states = [None] * max_size
         self.terminated = [None] * max_size
-    
-    def __len__(self):
+
+    def __len__(self) -> int:
+        """Return the number of items in memory.
+
+        Returns:
+            int: Size of memory.
+        """
         return self.size
     
-    def append(self, tup):
-        self.states[self.index] = tup[0]
-        self.actions[self.index] = tup[1]
-        self.rewards[self.index] = tup[2]
-        self.next_states[self.index] = tup[3]
-        self.terminated[self.index] = tup[4]
+    def append(self, experience: tuple) -> None:
+        """Add experience to memory.
+
+        Args:
+            experience (tuple): An experience containing state, action, reward, next_state, and
+            a termination flag.
+        """
+        self.states[self.index] = experience[0]
+        self.actions[self.index] = experience[1]
+        self.rewards[self.index] = experience[2]
+        self.next_states[self.index] = experience[3]
+        self.terminated[self.index] = experience[4]
         
         self.size = min(self.size + 1, self.max_size)
         self.index = (self.index + 1) % self.max_size
         
-    def sample(self, batch_size):
+    def sample(self, batch_size: int) -> list:
+        """Return a sample of experiences.
+
+        Args:
+            batch_size (int): Number of experiences sampled.
+
+        Returns:
+            list: A number of experiences given by a batch size.
+        """
         idx = self.rng.choice(self.size, batch_size, replace=False)
         
         return (
