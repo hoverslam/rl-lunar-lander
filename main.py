66b2faff5f50a3b25a597c6cf1c02747f8be0ad0
hoverslam <hofmann.christopher@outlook.com>
2022-08-27 00:04:33 +0200

    Add CLI

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import gym
 import sys
+import typer
 
 sys.path.append("modules")
 from modules.nfq import NFQAgent
 from modules.dqn import DQNAgent
 
 
+app = typer.Typer()
+
 agents = {"NFQ": NFQAgent(gamma=0.99, epsilon_init=1.0, epsilon_min=0.01, epsilon_decay=0.9, 
                           alpha=0.0001, input_dim=8, output_dim=4, hidden_dims=[1024, 512]),
           "DQN": DQNAgent(gamma=0.99, epsilon_init=1.0, epsilon_min=0.01, epsilon_decay=0.9, 
                           alpha=0.0001, input_dim=8, output_dim=4, hidden_dims=[1024, 512])}
 
-
+@app.command()
 def train(episodes: int, algo: str) -> None:
     env = gym.make("LunarLander-v2", new_step_api=True)
 
@@ -23,6 +26,7 @@ def train(episodes: int, algo: str) -> None:
 
     env.close()
     
+@app.command()
 def evaluate(episodes: int, algo: str, render: bool = True) -> None:
     if render:
         env = gym.make("LunarLander-v2", render_mode="human", new_step_api=True)
@@ -44,5 +48,4 @@ def evaluate(episodes: int, algo: str, render: bool = True) -> None:
     
 
 if __name__ == "__main__":
-    train(1000, "NFQ")
-    evaluate(10, "NFQ")
+    app()
